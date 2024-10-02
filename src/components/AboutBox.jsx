cbe7da9d1999e9764737dfa152960700af440fe3
Aumkitti <aumkitti.01@gmail.com>
2024-10-02 16:20:04 +0700

    first commit

---
+++ src/components/AboutBox.jsx
@@ -0,0 +1,78 @@
+import cv from "../assets/download/Resume Kittipong.pdf";
+
+const AboutBox = () => {
+  return (
+    <section className="section" id="about">
+      <div className="top-header">
+        <h1>About Me</h1>
+      </div>
+      <div className="row">
+        <div className="col">
+          <div className="about-info">
+            <h3>My introduction</h3>
+            <p>
+              Hello! I'm a passionate web developer with expertise in both
+              frontend and backend technologies. My skill set includes HTML,
+              CSS, Bootstrap, Tailwind, JavaScript, React, Node.js, Express.js,
+              Java, MySQL, PostgreSQL, Robot, and MongoDB. I enjoy creating dynamic and
+              responsive web applications that provide excellent user
+              experiences. Let's build something amazing together!
+            </p>
+            <div className="about-btn">
+              <a href={cv} download>
+                <button className="btn">
+                  Download Resume <i className="uil uil-import"></i>
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="skills-box">
+            <div className="skills-header">
+              <h3>Frontend</h3>
+            </div>
+            <div className="skills-list">
+              <span>HTML</span>
+              <span>CSS</span>
+              <span>Bootstrap</span>
+              <span>Tailwind</span>
+              <span>JavaScript</span>
+              <span>React</span>
+            </div>
+          </div>
+          <div className="skills-box">
+            <div className="skills-header">
+              <h3>Backend</h3>
+            </div>
+            <div className="skills-list">
+              <span>JAVA</span>
+              <span>Node.js</span>
+              <span>Express.js</span>
+            </div>
+          </div>
+          <div className="skills-box">
+            <div className="skills-header">
+              <h3>Database</h3>
+            </div>
+            <div className="skills-list">
+              <span>MySQL</span>
+              <span>PostgreSQL</span>
+              <span>MongoDB</span>
+            </div>
+          </div>
+          <div className="skills-box">
+            <div className="skills-header">
+              <h3>Tester</h3>
+            </div>
+            <div className="skills-list">
+              <span>Robot</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutBox;
---
+++ src/components/FeaturedBox.jsx
@@ -0,0 +1,70 @@
+import avatar from "../assets/images/Kitti.jpg";
+import cv from "../assets/download/Resume Kittipong.pdf";
+
+//หน้า 2
+
+const FeaturedBox = () => {
+  return (
+    <section className="featured-box" id="home">
+      <div className="featured-text">
+        <div className="featured-text-card">
+          <span>Kittipong Dachjit</span>
+        </div>
+        <div className="featured-name">
+          <p>
+            I'm <span className="typedText"></span>
+          </p>
+        </div>
+        <div className="featured-text-info">
+          <p>
+            Software engineering students interested in Frontend Developer are
+            looking for internship opportunities to use my knowledge to help
+            develop projects and want to gain experience. in knowledge
+            development
+          </p>
+        </div>
+        <div className="featured-text-btn">
+          <a href={cv} download>
+            <button className="btn">
+              Download Resume <i className="uil uil-file-alt"></i>
+            </button>
+          </a>
+        </div>
+        <div className="social_icons">
+          <div className="icon">
+            <a href="https://www.instagram.com/aum.ktpd/" target="_blank">
+              <i className="uil uil-instagram"></i>
+            </a>
+          </div>
+
+          <div className="icon">
+            <a
+              href="https://www.facebook.com/profile.php?id=100015293261179"
+              target="_blank"
+            >
+              <i className="uil uil-facebook"></i>
+            </a>
+          </div>
+          <div className="icon">
+            <a href="https://github.com/Aumkitti" target="_blank">
+              <i className="uil uil-github-alt"></i>
+            </a>{" "}
+          </div>
+        </div>
+      </div>
+      <div className="featured-image">
+        <div className="image">
+          <img src={avatar} alt="avatar" />
+        </div>
+      </div>
+      <div className="scroll-icon-box">
+        <a href="#about" className="scroll-btn">
+          <i className="uil uil-mouse-alt"></i>
+          <p>Scroll Down</p>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default FeaturedBox;
